33984da43666ddcb3df444c9aca466d74ad5fb12
Protophenomenal <frontczakf@gmail.com>
2025-01-22 12:43:14 +0100

    Start of Sprites lesson

--- main.py
+++ main.py
@@ -9,12 +9,17 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
+        clock.tick(60)
+        dt = (clock.tick(60)/1000)
+
     return 
 
 if __name__ == "__main__":
